f8f5be008f2ed758b7a2edc83875dbcf428dc313
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-07-11 13:28:38 +0330

    Implement Api

---
+++ Download-File-API/Controllers/FileController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace Download_File_API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+
+        FileExtensionContentTypeProvider fileExtensionContentTypeProvider;
+        public FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProviders)
+        {
+            fileExtensionContentTypeProvider = fileExtensionContentTypeProviders;
+        }
+
+
+        [HttpGet("{fileid}")]
+        public IActionResult GetFile(string fileid)
+        {
+            string pathToFile = "Files/"+fileid;
+            if(!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            var bytes=System.IO.File.ReadAllBytes(pathToFile);
+
+            if(!fileExtensionContentTypeProvider.TryGetContentType(pathToFile,out var contentType))
+            {
+                contentType = "application/actet-stream";
+            }
+
+            return File(bytes,contentType,Path.GetFileName(pathToFile));
+        }
+
+    }
+}
--- Download-File-API/Program.cs
+++ Download-File-API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region IOc
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+#endregion
+
 
 var app = builder.Build();
 
